ab0be85c11f7072aee1f94cc1bc0e4a5e679bbfa
andreh111 <andrehabboud1@gmail.com>
2023-06-14 05:48:25 +0400

    feat: added formik validation

--- __tests__/UserFormContainer.test.js
+++ __tests__/UserFormContainer.test.js
@@ -5,6 +5,7 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import UserFormContainer from '../src/components/UserFormContainer';
 import {login, register} from '../src/store/slices/auth.slice';
+import {act} from 'react-test-renderer';
 
 const mockStore = configureStore([thunk]);
 
@@ -39,15 +40,13 @@ describe('UserFormContainer', () => {
         <UserFormContainer mode="login" actionText="Log In" />
       </Provider>,
     );
+    await act(async () => {
+      fireEvent.changeText(getByTestId('email-input'), 'nilson@email.com');
+      fireEvent.changeText(getByTestId('password-input'), 'nilson');
+      fireEvent.press(getByText('Log In'));
+    });
 
-    fireEvent.changeText(getByTestId('email-input'), 'nilson@email.com');
-    fireEvent.changeText(getByTestId('password-input'), 'nilson');
-    fireEvent.press(getByText('Log In'));
-
-    const actions = store.getActions();
     const expectedPayload = {email: 'nilson@email.com', password: 'nilson'};
-
-    expect(actions[0].type).toEqual(login.pending.type);
 
     await store.dispatch(login(expectedPayload));
     const successAction = store
@@ -67,16 +66,15 @@ describe('UserFormContainer', () => {
       </Provider>,
     );
 
-    fireEvent.changeText(getByTestId('email-input'), 'andre@email.com');
-    fireEvent.changeText(getByTestId('password-input'), 'andre123');
-    fireEvent.press(getByText('Register'));
+    await act(async () => {
+      fireEvent.changeText(getByTestId('email-input'), 'dani@email.com');
+      fireEvent.changeText(getByTestId('password-input'), 'dani123');
+      fireEvent.press(getByText('Register'));
+    });
 
-    const actions = store.getActions();
-    const expectedPayload = {email: 'andre@email.com', password: 'andre123'};
+    const expectedPayload = {email: 'dani@email.com', password: 'dani123'};
 
-    expect(actions[0].type).toEqual(register.pending.type);
-
-    await store.dispatch(login(expectedPayload));
+    await store.dispatch(register(expectedPayload));
     const successAction = store
       .getActions()
       .find(action => action.type === register.fulfilled.type);
--- src/components/UserFormContainer.js
+++ src/components/UserFormContainer.js
@@ -1,20 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Button, Text, TextInput} from '@react-native-material/core';
 import {StyleSheet, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
+import {Formik} from 'formik';
 
 import {login, register} from '../store/slices/auth.slice';
+import {userValidationSchema} from '../validation/userValidationSchema';
 
 const UserFormContainer = ({mode, actionText}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const isLoading = useSelector(state => state.auth.isLoading);
   const error = useSelector(state => state.auth.error);
 
-  const handleAction = () => {
+  const handleAction = values => {
     dispatch(
-      mode === 'login' ? login({email, password}) : register({email, password}),
+      mode === 'login'
+        ? login({email: values.email, password: values.password})
+        : register({email: values.email, password: values.password}),
     );
   };
 
@@ -24,29 +26,53 @@ const UserFormContainer = ({mode, actionText}) => {
   return (
     <View style={[styles.container, styles.spacing]}>
       <Text style={styles.title}>{pageTitle}</Text>
-      <TextInput
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        style={styles.spacing}
-        testID="email-input"
-      />
-      <TextInput
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        style={styles.spacing}
-        testID="password-input"
-      />
-      <Button
-        title={actionText}
-        mode="contained"
-        onPress={handleAction}
-        style={styles.button}
-        loading={isLoading}
-      />
+      <Formik
+        initialValues={{email: '', password: ''}}
+        validationSchema={userValidationSchema}
+        onSubmit={handleAction}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <>
+            <TextInput
+              label="Email"
+              value={values.email}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              keyboardType="email-address"
+              style={styles.spacing}
+              testID="email-input"
+            />
+            {errors.email && touched.email && (
+              <Text style={styles.error}>{errors.email}</Text>
+            )}
+            <TextInput
+              label="Password"
+              value={values.password}
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              secureTextEntry
+              style={styles.spacing}
+              testID="password-input"
+            />
+            {errors.password && touched.password && (
+              <Text style={styles.error}>{errors.password}</Text>
+            )}
+            <Button
+              title={actionText}
+              mode="contained"
+              onPress={handleSubmit}
+              style={styles.button}
+              loading={isLoading}
+            />
+          </>
+        )}
+      </Formik>
       {error && (
         <Text style={[styles.error, styles.spacing]}>{error.message}</Text>
       )}
